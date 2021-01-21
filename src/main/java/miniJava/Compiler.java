0651d1da331911c97ea82662008fe28b2fa9a03c
Eric Schneider <u1643364@live.unc.edu>
2021-01-21 16:01:50 -0500

    More initial commit

---
+++ src/main/java/miniJava/Compiler.java
@@ -0,0 +1,11 @@
+package miniJava;
+
+public class Compiler {
+
+	public static void main(String[] args){
+		if(args.length == 0)
+			throw new IllegalArgumentException("Needs file to compile with!");
+		
+		// TODO
+	}
+}
