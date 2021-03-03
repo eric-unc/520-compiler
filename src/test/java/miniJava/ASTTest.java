8931f5508930d2faa6c76d0ebf5f3298077f043c
Eric Schneider <u1643364@live.unc.edu>
2021-03-03 01:59:00 -0500

    Expanded ASTTests a bit, although they are not especially useful

--- src/test/java/miniJava/ASTTest.java
+++ src/test/java/miniJava/ASTTest.java
@@ -15,6 +15,7 @@ import miniJava.SyntacticAnalyzer.Parser;
 import miniJava.SyntacticAnalyzer.Scanner;
 
 public class ASTTest {
+	// TODO: I really should do some kind of automatic text comparison but this is hard.
 
 	@Test
 	@FailOnSystemExit
@@ -25,7 +26,7 @@ public class ASTTest {
 			Parser parser = new Parser(scanner);
 			AST ast = parser.parse();
 			
-			ASTDisplay display = new ASTDisplay(); // TODO: maybe try to do some kind of text comparison
+			ASTDisplay display = new ASTDisplay(); 
 			display.showTree(ast);
 		}catch(FileNotFoundException e){
 			e.printStackTrace();
@@ -33,4 +34,37 @@ public class ASTTest {
 		}
 	}
 
+	@Test
+	@FailOnSystemExit
+	void test2(){
+		try {
+			FileInputStream stream = new FileInputStream(MainTest.RES + "Test2.mjava");
+			Scanner scanner = new Scanner(stream);
+			Parser parser = new Parser(scanner);
+			AST ast = parser.parse();
+			
+			ASTDisplay display = new ASTDisplay(); 
+			display.showTree(ast);
+		}catch(FileNotFoundException e){
+			e.printStackTrace();
+			fail();
+		}
+	}
+	
+	@Test
+	@FailOnSystemExit
+	void test3(){
+		try {
+			FileInputStream stream = new FileInputStream(MainTest.RES + "Test3.mjava");
+			Scanner scanner = new Scanner(stream);
+			Parser parser = new Parser(scanner);
+			AST ast = parser.parse();
+			
+			ASTDisplay display = new ASTDisplay(); 
+			display.showTree(ast);
+		}catch(FileNotFoundException e){
+			e.printStackTrace();
+			fail();
+		}
+	}
 }
